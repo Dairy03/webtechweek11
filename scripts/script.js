65ddbd346f310829cf5bf359c12178b44d7f533f
Darien <dlowe39@my.bcit.ca>
2023-03-22 12:46:08 -0700

    finishes the radius question in week 11 slides

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,40 @@
-function greet(x){
-    console.log('Hello ' + x);
+// function greet(x){
+//     console.log('Hello ' + x);
+// }
+
+// const greet = function (x){
+//     console.log('hello '+x);
+// }
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// let greet = (x) =>{
+//     console.log(`Hello ${x}`);
+// }
+
+
+// not recommended but works
+// name => console.log('hello'+x);
+
+
+//-----------------------------------------------------------------------------------------------------------------
+
+// function greet(name, time = "day"){
+//     console.log(`Good ${time}, ${name}.`)
+// }
+
+
+//---------------------------------------------------------------------------------------------------------------
+
+radius = prompt("enter radius");
+
+const makeCircle = () =>{
+    result = Math.PI * Math.pow(radius, 2);
+    alert(`The area of a circle with radius ${radius} is ${result}`);
+}
+
+if(isNaN(radius) == false){
+makeCircle()
+} else{
+    alert("The value entered is not a number.")
 }
